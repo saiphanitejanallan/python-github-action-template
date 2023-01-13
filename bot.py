8795eec07b9927d064f2cfa29c00cb95dfce5ebf
yoyobabycool <nspt.saiphaniteja@gmail.com>
2023-01-13 21:54:48 +0530

    tweet bot

--- bot.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-import os
-auth = tweepy.OAuthHandler("CONSUMER_KEY", os.getenv("TWITTER_CONSUMER_KEY"))
-auth.set_access_token("ACCESS_TOKEN", os.getenv("TWITTER_ACCESS_TOKEN"))
-api = tweepy.API(auth)
-current_time = datetime.datetime.now()
-api.update_status(status= current_time)
-print("Done!")
---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import tweepy
+auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
+auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+api = tweepy.API(auth)
+current_time = datetime.datetime.now()
+api.update_status(status= current_time)
+print("Done!")
