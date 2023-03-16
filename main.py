afd9445bea8da26f6230e3c42e0f6516a0f31cc9
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2023-03-16 23:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import tweepy
 import datetime
 import json
+
+import requests
 auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
 auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
